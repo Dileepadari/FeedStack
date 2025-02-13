18675ac885276d5a7f02afbc872fe4a93b8da05c
revanthsnr <revanthsnr@revanth>
2025-02-13 23:42:47 +0530

    Automated code refactoring
    Analyzed 5 files
    Found and fixed issues in 2 file pairs/singles

--- reader-web/src/main/java/com/sismics/reader/rest/resource/AppResource.java
+++ reader-web/src/main/java/com/sismics/reader/rest/resource/AppResource.java
@@ -1,7 +1,132 @@
+```java
+package com.sismics.reader.rest.resource;
+
+import com.sismics.reader.core.dao.jpa.FeedSubscriptionDao;
+import com.sismics.reader.core.dao.jpa.UserArticleDao;
+import com.sismics.reader.core.dao.jpa.criteria.FeedSubscriptionCriteria;
+import com.sismics.reader.core.dao.jpa.criteria.UserArticleCriteria;
+import com.sismics.reader.core.dao.jpa.dto.FeedSubscriptionDto;
+import com.sismics.reader.core.dao.jpa.dto.UserArticleDto;
+import com.sismics.reader.core.util.jpa.PaginatedList;
+import com.sismics.reader.core.util.jpa.PaginatedLists;
+import com.sismics.reader.rest.assembler.ArticleAssembler;
+import com.sismics.rest.exception.ClientException;
+import com.sismics.rest.exception.ForbiddenClientException;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * All articles REST resources.
+ *
+ * @author jtremeaux
+ */
+@Path("/all")
+public class AllResource extends BaseResource {
+    /**
+     * Returns all articles.
+     *
+     * @param unread Returns only unread articles
+     * @param limit  Page limit
+     * @param afterArticle Start the list after this user article
+     * @return Response
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Override
+    public Response get(
+            @QueryParam("unread") boolean unread,
+            @QueryParam("limit") Integer limit,
+            @QueryParam("after_article") String afterArticle) {
+        if (!authenticate()) {
+            throw new ForbiddenClientException();
+        }
+
+        UserArticleCriteria userArticleCriteria = new UserArticleCriteria()
+                .setUnread(unread)
+                .setUserId(principal.getId())
+                .setSubscribed(true)
+                .setVisible(true);
+        if (afterArticle != null) {
+            UserArticleCriteria afterArticleCriteria = new UserArticleCriteria()
+                    .setUserArticleId(afterArticle)
+                    .setUserId(principal.getId());
+            List<UserArticleDto> userArticleDtoList = userArticleDao.findByCriteria(afterArticleCriteria);
+            if (userArticleDtoList.isEmpty()) {
+                throw new ClientException("ArticleNotFound", MessageFormat.format("Can't find user article {0}", afterArticle));
+            }
+            UserArticleDto userArticleDto = userArticleDtoList.iterator().next();
+
+            userArticleCriteria.setArticlePublicationDateMax(new Date(userArticleDto.getArticlePublicationTimestamp()));
+            userArticleCriteria.setArticleIdMax(userArticleDto.getArticleId());
+        }
+
+        PaginatedList<UserArticleDto> paginatedList = PaginatedLists.create(limit, null);
+        userArticleDao.findByCriteria(paginatedList, userArticleCriteria, null, null);
+
+        List<JSONObject> articles = new ArrayList<JSONObject>();
+        for (UserArticleDto userArticle : paginatedList.getResultList()) {
+            articles.add(ArticleAssembler.asJson(userArticle));
+        }
+        JSONObject response = new JSONObject();
+        try {
+            response.put("articles", articles);
+        } catch (JSONException e) {
+            throw new ClientException("UnexpectedError", e);
+        }
+
+        return Response.ok().entity(response).build();
+    }
+
+    /**
+     * Marks all articles as read.
+     *
+     * @return Response
+     */
+    @POST
+    @Path("/read")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Override
+    public Response read() {
+        if (!authenticate()) {
+            throw new ForbiddenClientException();
+        }
+
+        UserArticleCriteria userArticleCriteria = new UserArticleCriteria()
+                .setUserId(principal.getId())
+                .setSubscribed(true);
+        userArticleDao.markAsRead(userArticleCriteria);
+
+        FeedSubscriptionCriteria feedSubscriptionCriteria = new FeedSubscriptionCriteria()
+                .setUserId(principal.getId());
+        for (FeedSubscriptionDto feedSubscrition : feedSubscriptionDao.findByCriteria(feedSubscriptionCriteria)) {
+            feedSubscriptionDao.updateUnreadCount(feedSubscrition.getId(), 0);
+        }
+
+        JSONObject response = new JSONObject();
+        try {
+            response.put("status", "ok");
+        } catch (JSONException e) {
+            throw new ClientException("UnexpectedError", e);
+        }
+        return Response.ok().entity(response).build();
+    }
+}
+```
+```java
 package com.sismics.reader.rest.resource;
 
 import com.sismics.reader.core.model.context.AppContext;
+import com.sismics.reader.core.service.IndexingService;
 import com.sismics.reader.core.util.ConfigUtil;
+import com.sismics.reader.core.util.NetworkUtil;
 import com.sismics.reader.core.util.jpa.PaginatedList;
 import com.sismics.reader.rest.constant.BaseFunction;
 import com.sismics.rest.exception.ForbiddenClientException;
@@ -39,6 +164,7 @@ public class AppResource extends BaseResource {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Override
     public Response version() throws JSONException {
         ResourceBundle configBundle = ConfigUtil.getConfigBundle();
         String currentVersion = configBundle.getString("api.current_version");
@@ -65,6 +191,7 @@ public class AppResource extends BaseResource {
     @GET
     @Path("log")
     @Produces(MediaType.APPLICATION_JSON)
+    @Override
     public Response log(@QueryParam("level") String level,
                         @QueryParam("tag") String tag,
                         @QueryParam("message") String message,
@@ -74,7 +201,6 @@ public class AppResource extends BaseResource {
             throw new ForbiddenClientException();
         }
         checkBaseFunction(BaseFunction.ADMIN);
-
         MemoryAppender memoryAppender = getMemoryAppender();
 
         LogCriteria logCriteria = new LogCriteria()
@@ -89,71 +215,4 @@ public class AppResource extends BaseResource {
         for (LogEntry logEntry : paginatedList.getResultList()) {
             JSONObject log = new JSONObject();
             log.put("date", logEntry.getTimestamp());
-            log.put("level", logEntry.getLevel());
-            log.put("tag", logEntry.getTag());
-            log.put("message", logEntry.getMessage());
-            logs.add(log);
-        }
-        response.put("total", paginatedList.getResultCount());
-        response.put("logs", logs);
-
-        return Response.ok().entity(response).build();
-    }
-
-    /**
-     * Destroy and rebuild articles index.
-     *
-     * @return Response
-     */
-    @POST
-    @Path("batch/reindex")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response batchReindex() throws JSONException {
-        if (!authenticate()) {
-            throw new ForbiddenClientException();
-        }
-        checkBaseFunction(BaseFunction.ADMIN);
-
-        JSONObject response = new JSONObject();
-        try {
-            AppContext.getInstance().getIndexingService().rebuildIndex();
-        } catch (Exception e) {
-            LOGGER.error("Error rebuilding index", e);
-            throw new ServerException("IndexingError", "Error rebuilding index", e);
-        }
-        response.put("status", "ok");
-        return Response.ok().entity(response).build();
-    }
-
-    /**
-     * Attempt to map a port to the gateway.
-     *
-     * @return Response
-     */
-    @POST
-    @Path("map_port")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response mapPort() throws JSONException {
-        if (!authenticate()) {
-            throw new ForbiddenClientException();
-        }
-        checkBaseFunction(BaseFunction.ADMIN);
-
-        JSONObject response = new JSONObject();
-        if (!NetworkUtil.mapTcpPort(request.getServerPort())) {
-            LOGGER.error("Error mapping port using UPnP");
-            throw new ServerException("NetworkError", "Error mapping port using UPnP");
-        }
-
-        response.put("status", "ok");
-        return Response.ok().entity(response).build();
-    }
-
-    private MemoryAppender getMemoryAppender() {
-        Appender appender = Logger.getRootLogger().getAppender("MEMORY");
-        if (appender == null || !(appender instanceof MemoryAppender)) {
-            throw new ServerException("ServerError", "MEMORY appender not configured");
-        }
-        return (MemoryAppender) appender;
-    }
-}
+            log.put("level",
